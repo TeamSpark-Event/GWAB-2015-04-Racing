c50014b0134ab1427d2942ff125bf5379c15e654
Boyko Anton <boyko.ant@live.com>
2015-04-25 00:24:00 +0300

    add recing.js

---
+++ racing.data.js
@@ -0,0 +1,157 @@
+﻿window.racing = window.racing || {};
+window.racing.data = window.racing.data || {};
+
+(function (racing) {
+
+    // Base URI for Racing Game Table Storage data.
+    var tableStorageBaseUrl = 'https://gabracinglab-secondary.table.core.windows.net/';
+
+    // Enumeration of the three tracks in the racing game.
+    var tracks = {
+        beginner: 0,
+        advanced: 1,
+        expert: 2
+    };
+
+    // Table names and shared access signatures for racing game data.
+    var auths = {
+        rankingLapTimes: {
+            tableName: 'RankingLapTimes',
+            signature: 'SLh2mtxVKdPj5d3%2FiI%2FMY2ymsHG9cZaLcRtLE9Yhf3E%3D'
+        },
+        achievements: {
+            tableName: 'Achievements',
+            signature: 'n6oDZu69sYu5fzWXHSon4nhSVAvVPrqfYcLwEw%2FHnHg%3D'
+        },
+        tracks: [
+            {
+                playerLapTimes: {
+                    tableName: 'PlayerLapTimes0',
+                    signature: '4Ykj7chtVfgd7CclQiFOScLTllBvgMqKjWle0AZYZ8Y%3D'
+                },
+                telemetryData: {
+                    tableName: 'TelemetryData0',
+                    signature: 'Nkanfxp98yMunfBD1B%2BBdmWI73k2RRD3WysKK5zmGBY%3D'
+                },
+                rankingLapTimesCountryTrack: {
+                    tableName: 'RankingLapTimesCountryTrack0',
+                    signature: 'Op2LIRuGot0fG1pnT%2FGtf7WoHJpcXyeas%2F0a221Yz9A%3D'
+                },
+                rankingLapTimesLocationTrack: {
+                    tableName: 'RankingLapTimesLocationTrack0',
+                    signature: 'Ep7kjKkEb%2FQPksTVfkzo8XpLJdNuy%2FqEN36PvVS1cls%3D'
+                }
+            },
+            {
+                playerLapTimes: {
+                    tableName: 'PlayerLapTimes1',
+                    signature: '3WgWajGbb0gRphGArFxkEYO6XqbnDHdXxMgYUzpWgGQ%3D'
+                },
+                telemetryData: {
+                    tableName: 'TelemetryData1',
+                    signature: 'jfDTmVYaf8iHEnkeQPBfwMOwq70HdNnKEa5YIEWUTiY%3D'
+                },
+                rankingLapTimesCountryTrack: {
+                    tableName: 'RankingLapTimesCountryTrack1',
+                    signature: 'yk6eICNAmwbBCnVsktGds4NvGwPxsU2Gs43G1fdyLmU%3D'
+                },
+                rankingLapTimesLocationTrack: {
+                    tableName: 'RankingLapTimesLocationTrack1',
+                    signature: 'ytwlA64Y2rENEn7lp5wQhKEgRzh0AHVBBIcCv7Ry1og%3D'
+                }
+            },
+            {
+                playerLapTimes: {
+                    tableName: 'PlayerLapTimes2',
+                    signature: 'L4htRc30fD9tUlHOz9T%2BvryvRYO5%2BM2uAj0uw012rXs%3D'
+                },
+                telemetryData: {
+                    tableName: 'TelemetryData2',
+                    signature: '%2FzmiXwrnNRmd3nWn97lw80cbyK%2BpvFQJrKDXIPd6V3Y%3D'
+                },
+                rankingLapTimesCountryTrack: {
+                    tableName: 'RankingLapTimesCountryTrack2',
+                    signature: '9r7358UlPUrw4foyS%2FOxykFNdi5tJZ%2FCQmQqk1P4Uew%3D'
+                },
+                rankingLapTimesLocationTrack: {
+                    tableName: 'RankingLapTimesLocationTrack2',
+                    signature: '2U%2FP7UmzBTLiqWmgFbl22IZiqY4Fhs7yCtpWPiwzl6E%3D'
+                }
+            }
+        ]
+    };
+
+    // Formats a query for a Table Storage partition.
+    function getPartitionFilter(value, prefix) {
+        var filter = {};
+        prefix = prefix || '';
+        if (typeof (value) !== 'undefined') {
+            filter.partitionKey = prefix + value;
+        }
+        return filter;
+    }
+
+
+  
+    // Sends a Get request to Table Storage, returning the JSON response.
+    function getJson(url, successCallback, errorCallback) {
+
+        // ToDo: Add code to get JSON data from a URI
+
+
+    }
+
+    
+    // Formats a URI for querying Table Storage using a Shared Access Signature
+    function getTableStorageUrl(auth, fields, filter) {
+
+        // ToDo: Add code to format a table storage URL with shared access signature
+
+
+    }
+
+
+
+    // Queries Table Storage and retrieves the data.
+    function getTableStorageData(auth, successCallback, fields, filter) {
+
+        // ToDo: Add code to get data from a Table Storage table
+
+
+    }
+
+
+
+    // Creates an object for retrieving data for a specified track.
+    function getTrack(track) {
+        return {
+
+            // ToDo: Add code to get data for the specific location
+
+
+            // ToDo: Add code to get data for the specific country
+
+
+            // ToDo: Add code to get the telemetry data for a specific lap
+
+
+        };
+    }
+
+
+
+    racing.data.getAchievements = function (successCallback, fields, countryCode) {
+
+        // ToDo: Add code to get the player achievements for your country
+
+
+    };
+
+
+
+    // Create the objects for the three tracks.
+    racing.data.beginnerTrack = getTrack(tracks.beginner);
+    racing.data.advancedTrack = getTrack(tracks.advanced);
+    racing.data.expertTrack = getTrack(tracks.expert);
+
+}(window.racing));
---
+++ racing.main.js
@@ -0,0 +1,170 @@
+﻿window.racing = window.racing || {};
+window.racing.app = window.racing.app || {};
+
+(function (racing) {
+
+    // Formats the lap time for display on the web page.
+    function formatMs(ms) {
+        function pad(n) {
+            return (n < 10 ? '0' : '') + n;
+        }
+
+        var milliseconds = ms % 1000;
+        var seconds = Math.floor((ms / 1000) % 60);
+        var minutes = Math.floor((ms / (60 * 1000)) % 60);
+
+        return pad(minutes) + ":" + pad(seconds) + "." + milliseconds;
+    }
+
+    // Creates a row in the results table with the appropriate values
+    function getTableRow(values, type, onClick) {
+        var row = document.createElement('tr');
+        values.forEach(function (value) {
+            var cell = document.createElement(type);
+            cell.innerText = value;
+            row.appendChild(cell);
+        });
+        if (onClick) {
+            row.addEventListener('click', onClick);
+        }
+        return row;
+    }
+
+
+    function getTelemetryEntries(data) {
+        return {
+            labels: ['Game Time', 'Speed', 'ERS Battery', 'Damage', 'Steering', 'Throttle', 'Brakes', 'ERS', 'Gear'],
+            data: data.map(function (entry) {
+                return [entry.GameTime, entry.Speed, entry.ErsBatteryPercent * 100, entry.Damage, (entry.SteeringAngle * 500) + 200, entry.ThrottlePercent * 100, 100 + entry.BrakesPercent * 100, 200 + (entry.HasUsedErs == true ? 100 : 0), entry.Gear * 50];
+            })
+        };
+    }
+
+    function renderLapTelemetry(title, data) {
+        var graphWrapper = document.querySelector('#TelemtryGraphWrapper');
+        var graphTitle = graphWrapper.querySelector('.title');
+        var graph = graphWrapper.querySelector('.graph');
+        var graphData = getTelemetryEntries(data);
+
+        graphTitle.innerText = title;
+        if (graphData.data.length) {
+            graphWrapper.style.display = 'block';
+
+            return new Dygraph(graph, graphData.data, {
+                labels: graphData.labels,
+                legend: 'always',
+                logscale: false
+            });
+        } else {
+            graphWrapper.style.display = 'none';
+        }
+    }
+
+    
+    function showLapTelemetry(lap, track) {
+
+        // ToDo: Add code to retrieve the telemetry data from storage and render a telemetry graph
+
+
+    }
+
+
+    // Renders the lap time data in the appropriate table on the results page.
+    function renderRankingLapTimes(laps, tableSelector, track, mode) {
+        var table = document.querySelector(tableSelector);
+        var tableHead = document.createElement('thead');
+        var tableBody = document.createElement('tbody');
+        var lapTimeZero = laps && laps.length > 0 ? laps[0].LapTimeMs : 0;
+
+        laps.forEach(function (lap, index) {
+            var lapTimeOffset = lap.LapTimeMs - lapTimeZero;
+
+            var cells;
+
+            switch (mode) {
+                case 'Country':
+                    cells = [(index + 1), lap.PlayerName, lap.Location, formatMs(lap.LapTimeMs), formatMs(lapTimeOffset), lap.Damage];
+                    break;
+                case 'Location':
+                    cells = [(index + 1), lap.PlayerName, formatMs(lap.LapTimeMs), formatMs(lapTimeOffset), lap.Damage];
+                    break;
+                default:
+                    cells = [(index + 1), lap.PlayerName, lap.Country, lap.Location, formatMs(lap.LapTimeMs), formatMs(lapTimeOffset), lap.Damage];
+                    break;
+            }
+
+            tableBody.appendChild(getTableRow(cells, 'td', function () {
+                showLapTelemetry(lap, track);
+            }));
+
+        });
+
+        table.innerHTML = '';
+
+        switch (mode) {
+            case 'Country':
+                tableHead.appendChild(getTableRow(['Pos', 'Name', 'Location', 'Laptime', 'Delta', 'Damage'], 'th'));
+                break;
+            case 'Location':
+                tableHead.appendChild(getTableRow(['Pos', 'Name', 'Laptime', 'Delta', 'Damage'], 'th'));
+                break;
+            default:
+                tableHead.appendChild(getTableRow(['Pos', 'Name', 'Country', 'Location', 'Laptime', 'Delta', 'Damage'], 'th'));
+                break;
+        }
+
+        table.appendChild(tableHead);
+        table.appendChild(tableBody);
+    }
+
+
+
+    // Country Statistics
+    function initCountryStat(track, selector, countryCode) {
+
+        // ToDo: Add code to get the country track data and pass it to the render method
+
+
+    }
+
+    function initCountryStats(countryCode) {
+
+        // ToDo: Add code to initialize the country data for the three tracks
+
+
+    }
+
+    // Location Statistics
+
+    // Get the track data and pass it to the render method
+    function initLocationStat(track, selector, locationId) {
+
+        // ToDo: Add code to get the location track data and pass it to the render method
+
+
+    }
+
+
+
+    // Initialize the data for the tracks.
+    function initLocationStats(locationId) {
+
+        // ToDo: Add code to initialize the location data for the beginner track
+
+
+        // ToDo: Add code to initialize the location data for the advanced and expert tracks
+
+
+    }
+
+
+
+    // ToDo: Set your country code and location ID.
+    var yourLocationId = 'YOUR_LOCATION_ID';
+    var yourCountryCode = 'YOUR_COUNTRY_CODE';
+
+    // Initialize the page with location statistics
+    initLocationStats(yourLocationId);
+    initCountryStats(yourCountryCode);
+
+}(window.racing));
